1f309c779b568f6ce1dd604350476d117df75a1a
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2026-02-13 08:24:04 +0100

    homepodctl v0.1.3

--- Formula/homepodctl.rb
+++ Formula/homepodctl.rb
@@ -1,16 +1,16 @@
 class Homepodctl < Formula
   desc "macOS CLI for Apple Music + HomePod control"
   homepage "https://github.com/agisilaos/homepodctl"
-  version "0.1.2"
+  version "0.1.3"
 
   on_macos do
     on_arm do
-      url "https://github.com/agisilaos/homepodctl/releases/download/v0.1.2/homepodctl_v0.1.2_darwin_arm64.tar.gz"
-      sha256 "927e7c7ce64bb2f7514ff697b4a32d4810bf14e1398536e368af7490d3f72ee5"
+      url "https://github.com/agisilaos/homepodctl/releases/download/v0.1.3/homepodctl_v0.1.3_darwin_arm64.tar.gz"
+      sha256 "b04e06b387de7096509ed9dbbf79aee697f643855047fac66f3402b3fa8ca3d9"
     end
     on_intel do
-      url "https://github.com/agisilaos/homepodctl/releases/download/v0.1.2/homepodctl_v0.1.2_darwin_amd64.tar.gz"
-      sha256 "67410477a73fb74cc956c3f2a0bd773b0189f69c275f7fa923789fd96f8e4c63"
+      url "https://github.com/agisilaos/homepodctl/releases/download/v0.1.3/homepodctl_v0.1.3_darwin_amd64.tar.gz"
+      sha256 "8ab8ca6b05a2286d5735668013887684b3ae302a4becf0b7a85d829406eae170"
     end
   end
 
--- README.md
+++ README.md
@@ -12,6 +12,7 @@ Homebrew formulae for Agis' CLI tools.
 
 
 
+
 ## Install
 
 ```bash
