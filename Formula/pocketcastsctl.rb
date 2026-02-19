7f1be1ef43d20ce739a8734ef4c574899519f2eb
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2026-02-19 16:04:02 +0100

    pocketcastsctl v0.1.5

--- Formula/pocketcastsctl.rb
+++ Formula/pocketcastsctl.rb
@@ -1,16 +1,16 @@
 class Pocketcastsctl < Formula
   desc "macOS CLI for Pocket Casts Web Player control"
   homepage "https://github.com/agisilaos/pocketcastsctl"
-  version "0.1.4"
+  version "0.1.5"
 
   on_macos do
     on_arm do
-      url "https://github.com/agisilaos/pocketcastsctl/releases/download/v0.1.4/pocketcastsctl_v0.1.4_darwin_arm64.tar.gz"
-      sha256 "1a1d97301b07719ca193bb3a1abd7f66f5390c4e3c553270b1771157d3d77f3e"
+      url "https://github.com/agisilaos/pocketcastsctl/releases/download/v0.1.5/pocketcastsctl_v0.1.5_darwin_arm64.tar.gz"
+      sha256 "aaa96838320a9b530719179684fe67048cf7cf5e386e1b56a0e6c712e8997113"
     end
     on_intel do
-      url "https://github.com/agisilaos/pocketcastsctl/releases/download/v0.1.4/pocketcastsctl_v0.1.4_darwin_amd64.tar.gz"
-      sha256 "5771900f694a66c4cd374fb430be3b5f784dec89eb8a2c5fb242aabe9a22eee0"
+      url "https://github.com/agisilaos/pocketcastsctl/releases/download/v0.1.5/pocketcastsctl_v0.1.5_darwin_amd64.tar.gz"
+      sha256 "f9c9ff1b7aac5c5963824f8c709b31d8b5c495988c93d402a1484a373e6231d2"
     end
   end
 
--- README.md
+++ README.md
@@ -22,6 +22,7 @@ Homebrew formulae for Agis' CLI tools.
 
 
 
+
 ## Install
 
 ```bash
