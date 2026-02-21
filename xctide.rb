a430a85afb03c44ebf8e4007c83813fed2040928
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2026-02-21 21:54:51 +0100

    xctide: v0.1.1

--- xctide.rb
+++ xctide.rb
@@ -2,15 +2,15 @@ class Xctide < Formula
   desc "xctide command-line tool"
   homepage "https://github.com/agisilaos/xctide"
   license "MIT"
-  version "0.1.0"
+  version "0.1.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/agisilaos/xctide/releases/download/v0.1.0/xctide_0.1.0_darwin_arm64.tar.gz"
-      sha256 "1fc561c5400ea05528347f443097d8a1b33b32a54db24c69103b6acfe9e6275e"
+      url "https://github.com/agisilaos/xctide/releases/download/v0.1.1/xctide_0.1.1_darwin_arm64.tar.gz"
+      sha256 "931e92f2094776daa650113bbde0cd1fd5e9fc519e82e49644e4a552a9b2a836"
     else
-      url "https://github.com/agisilaos/xctide/releases/download/v0.1.0/xctide_0.1.0_darwin_amd64.tar.gz"
-      sha256 "81708b142e59bc854fa92695ad24d7762afdb4a6d12a88adf509756b8ad7f800"
+      url "https://github.com/agisilaos/xctide/releases/download/v0.1.1/xctide_0.1.1_darwin_amd64.tar.gz"
+      sha256 "6f4c2dfcda005b9953f4dacf93a0db3f081a58cd54d9f3ff50f2412e59800473"
     end
   end
 
