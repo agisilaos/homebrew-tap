605f3c1ea30b7669d46544f07c0a878515f86d68
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2026-02-22 18:41:52 +0100

    todoist-cli: v0.6.1

--- Formula/todoist-cli.rb
+++ Formula/todoist-cli.rb
@@ -1,16 +1,16 @@
-class TodoistCli < Formula
+class Todoist Cli < Formula
   desc "Agentic CLI for Todoist"
   homepage "https://github.com/agisilaos/todoist-cli"
-  version "0.6.0"
+  license "MIT"
+  version "0.6.1"
 
   on_macos do
-    on_arm do
-      url "https://github.com/agisilaos/todoist-cli/releases/download/v0.6.0/todoist-cli_v0.6.0_darwin_arm64.tar.gz"
-      sha256 "d0a2f45fccecc3e24b487cd2777f6f511d0efba3f4b35ff7522b9697b1083e2d"
-    end
-    on_intel do
-      url "https://github.com/agisilaos/todoist-cli/releases/download/v0.6.0/todoist-cli_v0.6.0_darwin_amd64.tar.gz"
-      sha256 "2c325f4cfa4c0de1326e89ed8ae72bda2dcecfd0a7087748d08b302acacad275"
+    if Hardware::CPU.arm?
+      url "https://github.com/agisilaos/todoist-cli/releases/download/v0.6.1/todoist-cli_0.6.1_darwin_arm64.tar.gz"
+      sha256 "1d7fea20b9adb2b83ec7ff274378bc08c581cf13ee64600502210440d97ab8c8"
+    else
+      url "https://github.com/agisilaos/todoist-cli/releases/download/v0.6.1/todoist-cli_0.6.1_darwin_amd64.tar.gz"
+      sha256 "f528371308fb220180e0bc8c40dc58b908db3766161434f4030c01408ab57fdc"
     end
   end
 
@@ -19,6 +19,6 @@ class TodoistCli < Formula
   end
 
   test do
-    system "#{bin}/todoist", "--version"
+    shell_output("#{bin}/todoist --version")
   end
 end
