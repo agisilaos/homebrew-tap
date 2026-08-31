784641a91309f1221cea3ae8d0e2d7fff4c6af4f
Agisilaos Tsarampoulidis <agtsaraboulidis@gmail.com>
2026-08-31 14:50:37 +0200

    homepodctl: v0.3.0

--- Formula/homepodctl.rb
+++ Formula/homepodctl.rb
@@ -2,15 +2,15 @@ class Homepodctl < Formula
   desc "macOS CLI for Apple Music + HomePod control"
   homepage "https://github.com/agisilaos/homepodctl"
   license "MIT"
-  version "0.2.0"
+  version "0.3.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/agisilaos/homepodctl/releases/download/v0.2.0/homepodctl_0.2.0_darwin_arm64.tar.gz"
-      sha256 "df5aade5ab8ee6d73070ae55894d9185851f3eeaca77257c5477848257177089"
+      url "https://github.com/agisilaos/homepodctl/releases/download/v0.3.0/homepodctl_0.3.0_darwin_arm64.tar.gz"
+      sha256 "eccc9b18c1e3192be4c5b31179c52bd62efc4d83d9a91a0140410bb5a270cda3"
     else
-      url "https://github.com/agisilaos/homepodctl/releases/download/v0.2.0/homepodctl_0.2.0_darwin_amd64.tar.gz"
-      sha256 "6bcfb8a0340291a639379d15a790c004a2456f93c0f89ae3d6385615af3a9ed5"
+      url "https://github.com/agisilaos/homepodctl/releases/download/v0.3.0/homepodctl_0.3.0_darwin_amd64.tar.gz"
+      sha256 "dece15aebcca73eb405dc605104f2a4f997a8e6b3b8ed3958fb6c2ad8a8153f9"
     end
   end
 
